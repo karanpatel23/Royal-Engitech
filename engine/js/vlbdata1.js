91ad123ec19792e7187fea4f7c6af4f6ceb3d41d
Karan Patel <114190916+karanpatel23@users.noreply.github.com>
2026-06-03 12:36:32 -0400

    Add files via upload

---
+++ engine/js/vlbdata1.js
@@ -0,0 +1,3 @@
+var $jt = $.noConflict();
+
+$jt(document).ready(function(){ window.Lightbox = new $jt().visualLightbox({autoPlay:true,borderSize:21,classNames:'vlightbox1',descSliding:true,enableRightClick:false,enableSlideshow:true,overlayOpacity:0.7,resizeSpeed:7,slideTime:4,startZoom:true}) });
